17d139b0d337ffd27a763b4fe7bfffa30cf6db8a
baigashad-in <baigashad.in@gmail.com>
2026-03-26 23:03:58 +0000

    Prometheus Settings

--- core/metrics.py
+++ core/metrics.py
@@ -1,1 +1,19 @@
 from prometheus_client import Counter, Gauge, Histogram, CollectorRegistry
+
+# Use default registry for metrics
+REGISTRY = CollectorRegistry(auto_describe=True)
+
+# --- Counters (always go up) ------------------------------------
+
+tasks_submitted_total = Counter(
+    "queueflow_tasks_submitted_total",
+    "Total number of tasks submitted to the queue",
+    ["task_name", "priority"],
+)
+
+tasks_completed_total = Counter(
+    "queueflow_tasks_completed_total",
+    "Total number of tasks completed successfully",
+    ["task_name", "priority"],
+)
+
